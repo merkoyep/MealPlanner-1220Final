6f6764d1325386bbf983fbaa266530c795a38912
merkoyep <81783308+merkoyep@users.noreply.github.com>
2024-10-04 01:50:48 -0400

    config attempt

--- app/config.py
+++ app/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 class Config(object):
     """Set environment variables."""
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", 'postgresql://postgres:5b2e05cd8c3526e3@merkoyep-final-project-db.dev.merkoyep.me:5432/postgres')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'itsasecret')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
